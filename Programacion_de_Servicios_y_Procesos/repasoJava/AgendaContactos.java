3b855755fc787c5670c82f358deab6b7498d123b
Yussef P <jose_88alb@hotmail.com>
2026-09-17 21:08:40 +0200

    codificación AgendaContactos.java

--- Programacion_de_Servicios_y_Procesos/repasoJava/AgendaContactos.java
+++ Programacion_de_Servicios_y_Procesos/repasoJava/AgendaContactos.java
@@ -1,5 +1,54 @@
-/* Identifica cada clase y método para la agenda de contactos, ¿Debe llevar base de datos con SQLite3? */
+import java.util.Scanner;
+import java.util.TreeSet;
 
 public class AgendaContactos {
     
+    public static TreeSet<Contacto> agenda = new TreeSet<>();
+
+    public static void añadeContacto(Contacto nuevo) {
+
+        try {
+            boolean realizado = agenda.add(nuevo);
+
+            if(realizado) {
+                System.out.println("¡Contacto añadido!");
+            } else {
+                System.out.println("¡Error!, el contacto ya existe en la agenda.");
+            }
+            
+        } catch (Exception e) {
+            System.out.println(e + "\nError, contacto erroneo.");
+        }
+    }
+
+    public static void borraContacto(Contacto guardado) {
+
+        try {
+            boolean borrado = agenda.remove(guardado);
+
+            if(borrado) {
+                System.out.println("¡Contacto borrado!");
+            } else {
+                System.out.println("¡Error!, el contacto no existe en la agenda.");
+            }
+
+        } catch (Exception e) {
+            System.out.println(e + "\nError, contacto erroneo2.");
+        }
+    }
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(" ===========================");
+        System.out.println("     AGENDA DE CONTACTOS    ");
+        System.out.println(" ===========================");
+        System.out.println("   1- Añadir contacto");
+        System.out.println("   2- Buscar contacto");
+        System.out.println("   3- Modificar contacto");
+        System.out.println("   4- Eliminar contacto");
+        System.out.println("   5- Salir");
+
+    }
 }
